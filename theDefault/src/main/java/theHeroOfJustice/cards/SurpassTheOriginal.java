4eb34c19a96e6314f5164cd94190c3c3fa11e66f
Justin Z <jzauds@gmail.com>
2023-03-10 20:44:42 -0500

    Added "Surpass" card

---
+++ theDefault/src/main/java/theHeroOfJustice/cards/SurpassTheOriginal.java
@@ -0,0 +1,75 @@
+package theHeroOfJustice.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import theHeroOfJustice.DefaultMod;
+import theHeroOfJustice.characters.TheDefault;
+
+import java.util.ArrayList;
+
+import static theHeroOfJustice.DefaultMod.makeCardPath;
+
+public class SurpassTheOriginal extends AbstractDynamicCard {
+
+    /*
+     * Wiki-page: https://github.com/daviscook477/BaseMod/wiki/Custom-Cards
+     *
+     * Surpass the Original - Deal X damage. Gain X Strength for each buff on target enemy.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID(SurpassTheOriginal.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int DAMAGE = 8;
+    private static final int UPGRADE_PLUS_DMG = 3;
+    private static final int MAGIC = 2;
+
+    // /STAT DECLARATION/
+
+    public SurpassTheOriginal() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        magicNumber = baseMagicNumber = MAGIC;
+        baseDamage = DAMAGE;
+    }
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HEAVY));
+        for(int i = 0; i < m.powers.size(); i++){
+            if(m.powers.get(i).type.equals(AbstractPower.PowerType.BUFF))
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new StrengthPower(p, 2)));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            initializeDescription();
+        }
+    }
+}
