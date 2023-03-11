0488f7ec8b1b4ac06373cf07cafde56790765fe8
Justin Z <jzauds@gmail.com>
2023-03-10 20:45:02 -0500

    Changed cost on "Structural Mastery" from 2 to 1

--- theDefault/src/main/java/theHeroOfJustice/cards/StructuralMastery.java
+++ theDefault/src/main/java/theHeroOfJustice/cards/StructuralMastery.java
@@ -39,7 +39,7 @@ public class StructuralMastery extends AbstractDynamicCard {
     private static final CardType TYPE = CardType.POWER;
     public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
 
-    private static final int COST = 2;
+    private static final int COST = 1;
     private static final int MAGIC = 1;
 
     // /STAT DECLARATION/
